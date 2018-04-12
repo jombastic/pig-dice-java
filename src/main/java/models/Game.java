5a889f03d3912c14d09b8702934cc83df7e0e75b
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-04-12 13:07:26 +0200

    Add randomDice method that generates a random number between 1 and 6

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -1,9 +1,16 @@
 package models;
 
+import java.util.Random;
+
 public class Game {
   private String currentPlayer;
 
   public Game(String currentPlayer) {
     this.currentPlayer = currentPlayer;
+  }
+
+  public int randomDice() {
+    Random random = new Random();
+    return random.nextInt((6 - 1) + 1) + 1;
   }
 }
--- src/test/java/models/GameTest.java
+++ src/test/java/models/GameTest.java
@@ -2,6 +2,9 @@ package models;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class GameTest {
@@ -9,5 +12,11 @@ public class GameTest {
   public void Game_createGameInstance() {
     Game testGame = new Game("Player 1");
     assertEquals(true, testGame instanceof Game);
+  }
+
+  @Test
+  public void randomDice_generateRandomDiceNumber_int() {
+    Game testGame = new Game("Player 1");
+    assertEquals(true, testGame.randomDice() != 0);
   }
 }
